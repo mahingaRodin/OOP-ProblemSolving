3db7adc4b9d15466b60fc805324bcf6a07c3701d
P-Odiix <mahingarodin@gmail.com>
2025-01-06 22:19:31 +0200

    "initialized the Main.java and lms with the book class"

---
+++ src/libraryManagementSystem/lms.java
@@ -0,0 +1,29 @@
+package libraryManagementSystem;
+
+//initializing the book class
+class Book {
+    private String title;
+    private String author;
+    private boolean isIssued;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.isIssued = false;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isIssued() {
+        return  isIssued;
+    }
+}
+
+
+public class lms {
+}

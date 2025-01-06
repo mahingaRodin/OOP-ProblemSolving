f761deca133187518cb4d10acd63e829226e950f
P-Odiix <mahingarodin@gmail.com>
2025-01-06 22:30:09 +0200

    "added the Library class with methods to be carried on the books"

--- src/libraryManagementSystem/lms.java
+++ src/libraryManagementSystem/lms.java
@@ -1,5 +1,8 @@
 package libraryManagementSystem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //initializing the book class
 class Book {
     private String title;
@@ -24,6 +27,34 @@ class Book {
     }
 }
 
+//library class
+
+class Library {
+    private List<Book> books = new ArrayList<>();
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+    public void removeBook(String title) {
+        books.removeIf(book -> book.getTitle().equals(title));
+    }
+    public Book searchItem(String title) {
+        for(Book book : books) {
+            if(book.getTitle().equals(title) && !book.isIssued()) {
+                return book;
+            }
+        }
+        return  null;
+    }
+
+    public void displayAllBooks() {
+        for (Book book : books ) {
+            if(!book.isIssued()) {
+                System.out.println(book.getTitle() + " by " + book.getAuthor());
+            }
+        }
+    }
+}
 
 public class lms {
 }

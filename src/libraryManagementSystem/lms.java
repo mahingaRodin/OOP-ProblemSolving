8fe5e3a3b21423e61766b61578150842720a0291
P-Odiix <mahingarodin@gmail.com>
2025-01-07 08:56:19 +0200

    "add"

--- src/libraryManagementSystem/lms.java
+++ src/libraryManagementSystem/lms.java
@@ -32,7 +32,7 @@ class Book {
 class Library {
     private List<Book> books = new ArrayList<>();
 
-    //library method
+    //library methods
     public void addBook(Book book) {
         books.add(book);
     }

355d45e349abc0a4a6d04f80b304460fb9c0c0c7
P-Odiix <mahingarodin@gmail.com>
2025-01-13 11:49:45 +0200

    "extended the thread class in boys"

--- src/functionalinterface/sittingArrangement/Boys.java
+++ src/functionalinterface/sittingArrangement/Boys.java
@@ -1,6 +1,6 @@
 package functionalinterface.sittingArrangement;
 
-public class Boys {
+public class Boys extends Thread{
     public void sitting() {
         for(int i=0; i<100; i++) {
             System.out.println("Boy");

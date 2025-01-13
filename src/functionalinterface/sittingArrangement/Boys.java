29bd55264f5c72ef8344133e1355402321646912
P-Odiix <mahingarodin@gmail.com>
2025-01-13 12:04:04 +0200

    "implemented the runnable interface"

--- src/functionalinterface/sittingArrangement/Boys.java
+++ src/functionalinterface/sittingArrangement/Boys.java
@@ -1,6 +1,6 @@
 package functionalinterface.sittingArrangement;
 
-public class Boys extends Thread{
+public class Boys implements Runnable{
     public void run() {
         for(int i=0; i<100; i++) {
             System.out.println("Boy");

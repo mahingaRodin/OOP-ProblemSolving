7fc3fb866bf23d2ad38ad852fe474396b9160260
P-Odiix <mahingarodin@gmail.com>
2025-01-13 11:37:12 +0200

    "updated the codes to return"

--- src/functionalinterface/Printer.java
+++ src/functionalinterface/Printer.java
@@ -1,5 +1,5 @@
 package functionalinterface;
 
 public interface Printer {
-    abstract void display(int n1 , int n2);
+    abstract int display(int n1 , int n2);
 }
--- src/functionalinterface/PrinterMain.java
+++ src/functionalinterface/PrinterMain.java
@@ -2,7 +2,9 @@ package functionalinterface;
 
 public class PrinterMain{
     public static void main(String[] args) {
-        Printer p = (n1, n2) -> System.out.println(n1+n2);
-        p.display(20, 20);
+        Printer p = (n1, n2) -> n1+n2;
+
+        int sum = p.display(20,49);
+        System.out.println(sum);
     }
 }

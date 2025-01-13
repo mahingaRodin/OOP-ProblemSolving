ebfa4398629ad5e00075e564f7990f07f6b0026c
P-Odiix <mahingarodin@gmail.com>
2025-01-13 11:57:36 +0200

    "called the sleep method to run the sitting plan in parallel"

--- src/functionalinterface/sittingArrangement/Boys.java
+++ src/functionalinterface/sittingArrangement/Boys.java
@@ -4,6 +4,11 @@ public class Boys extends Thread{
     public void run() {
         for(int i=0; i<100; i++) {
             System.out.println("Boy");
+            try {
+                Thread.sleep(5);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
--- src/functionalinterface/sittingArrangement/Girls.java
+++ src/functionalinterface/sittingArrangement/Girls.java
@@ -4,6 +4,11 @@ public class Girls extends Thread {
     public void run() {
         for(int i=0; i<100; i++) {
             System.out.println("Girl");
+            try {
+                Thread.sleep(5);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }

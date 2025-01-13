da8b3dc712b2855e5599c6d5415cfe324c6a7c27
P-Odiix <mahingarodin@gmail.com>
2025-01-13 11:51:15 +0200

    "overrode run in Boys.java"

--- src/functionalinterface/sittingArrangement/Boys.java
+++ src/functionalinterface/sittingArrangement/Boys.java
@@ -1,7 +1,7 @@
 package functionalinterface.sittingArrangement;
 
 public class Boys extends Thread{
-    public void sitting() {
+    public void run() {
         for(int i=0; i<100; i++) {
             System.out.println("Boy");
         }

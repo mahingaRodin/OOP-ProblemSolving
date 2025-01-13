8efbf7e81da5f94b635dffe4e0e66994de5e9bb3
P-Odiix <mahingarodin@gmail.com>
2025-01-13 11:13:38 +0200

    "implemented the lambda expression inside the PrinterMain.java"

--- src/functionalinterface/PrinterMain.java
+++ src/functionalinterface/PrinterMain.java
@@ -2,12 +2,7 @@ package functionalinterface;
 
 public class PrinterMain{
     public static void main(String[] args) {
-        Printer p = new Printer() {
-            @Override
-            public void display() {
-                System.out.println("the implementation");
-            }
-        };
+        Printer p = () -> System.out.println("the implementation");
         p.display();
     }
 }

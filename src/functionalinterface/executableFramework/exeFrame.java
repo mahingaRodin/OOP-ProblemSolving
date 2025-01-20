2bf63eea96e8bfdb9fd5945014ee5fb7b2d3d0ff
P-Odiix <mahingarodin@gmail.com>
2025-01-20 11:42:53 +0200

    "added the boy and girl class in the exeFrame"

--- src/functionalinterface/executableFramework/exeFrame.java
+++ src/functionalinterface/executableFramework/exeFrame.java
@@ -17,7 +17,21 @@ class BoyA implements Runnable {
     }
 }
 
-
+class GirlA implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Girl thread has started. ");
+        for(int i=0; i<=100; i++) {
+            System.out.println("Girl: "+i);
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println("Girl thread has completed!");
+    }
+}
 
 
 public class exeFrame {

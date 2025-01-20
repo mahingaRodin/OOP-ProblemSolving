f0594331564240131a89883e0c13998732659316
P-Odiix <mahingarodin@gmail.com>
2025-01-20 12:07:44 +0200

    "added the newFixedThreadPool"

--- src/functionalinterface/executableFramework/exeFrame.java
+++ src/functionalinterface/executableFramework/exeFrame.java
@@ -36,12 +36,32 @@ class GirlA implements Runnable {
     }
 }
 
+class TeacherA implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Teacher thread has started. ");
+        for(int i=0; i<=100; i++) {
+            System.out.println("Teacher: "+i);
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println("Teacher thread has completed!");
+    }
+    }
+
 
 public class exeFrame {
     public static void main(String[] args) {
-        ExecutorService executor = Executors.newSingleThreadExecutor();
+        ExecutorService executor = Executors.newFixedThreadPool(3);
         executor.execute(new BoyA());
         executor.execute(new GirlA());
+        executor.execute(new TeacherA());
         executor.shutdown();
+        for(int i=0; i<5; i++) {
+            System.out.println("main values: "+i );
+        }
     }
 }

81c3a9a2136bb25ab981e1410020cd27546d6b35
P-Odiix <mahingarodin@gmail.com>
2025-01-20 10:45:17 +0200

    "updated the main class"

--- src/functionalinterface/bankApp/BankAccount.java
+++ src/functionalinterface/bankApp/BankAccount.java
@@ -1,28 +1,34 @@
 package functionalinterface.bankApp;
 
 public class BankAccount {
-    public Deposit deposit;
     private double balance;
+
     public BankAccount(double initialBalance) {
         this.balance = initialBalance;
     }
 
     public static void main(String[] args) {
-    BankAccount bankAccount = new BankAccount(200000);
+        BankAccount bankAccount = new BankAccount(200000);
+
+        // Using lambda expressions for thread creation
+        Runnable withdrawTask = () -> new Withdraw(bankAccount, 500).start();
+        Runnable depositTask = () -> new Deposit(bankAccount, 300).start();
+
+        new Thread(withdrawTask).start();
+        new Thread(depositTask).start();
     }
 
     public synchronized void deposit(double amount) {
-        this.balance += amount;
+        balance += amount;
+        System.out.println("Deposited: " + amount + ", New Balance: " + balance);
     }
 
     public synchronized void withdraw(int amount) {
-        if(balance >= amount) {
-            this.balance -= amount;
+        if (balance >= amount) {
+            balance -= amount;
+            System.out.println("Withdrew: " + amount + ", New Balance: " + balance);
         } else {
             System.out.println("Insufficient balance");
         }
     }
-
-
-
 }
